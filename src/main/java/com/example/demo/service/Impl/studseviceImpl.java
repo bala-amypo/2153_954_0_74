44b1ddc303cfa211943591e3f82ed9f3eb423fcf
bala-amypo <bala@amypo.in>
2025-12-17 06:18:39 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/studseviceImpl.java
+++ src/main/java/com/example/demo/service/Impl/studseviceImpl.java
@@ -9,5 +9,22 @@ import com.example.demo.entity.Studentity;
 public class StudserviceImpl implements Studservice{
     private final Map<Long,Studentity>store = new HashMap<>();
 
-    @Ov
+    @Override
+    public Studentity insertStudentity(Studentity st){
+        store.put(st.getId(),st);
+        return st;
+    }
+
+    @Overridepublic List<Studentity>getAllStudentity(){
+        return new
+        ArrayList<>(store.values());
+    }
+
+    @Overridepublic Optinal<Studentity>getOneStudent(Long id){
+        return Optinal.ofNullable(store.get(id));
+    }
+    @Override
+    public void deleteStudent(Long id){
+        store.remove(id);
+    }
 }
